eb081fcbf2556328f5e944abf622958ba50b05c3
Michal Rozek <michalrozek90@gmail.com>
2023-07-08 22:53:51 +0200

    warlock destro rotation improvement

--- AIO/Combat/Warlock/SoloDestruction.cs
+++ AIO/Combat/Warlock/SoloDestruction.cs
@@ -18,12 +18,13 @@ namespace AIO.Combat.Warlock
             new RotationStep(new RotationSpell("Life Tap"), 3.1f, (s,t) => Settings.Current.GlyphLifeTap && !Me.HaveBuff("Life Tap") && Me.HealthPercent > 25, RotationCombatUtil.FindMe),
             new RotationStep(new RotationSpell("Curse of the Elements"), 3.2f, (s,t) => !t.HaveBuff("Curse of the Elements"), RotationCombatUtil.BotTarget),
             new RotationStep(new RotationSpell("Rain of Fire"), 3.5f, (s,t) => Me.IsInGroup && RotationFramework.Enemies.Count(o => o.IsTargetingMeOrMyPetOrPartyMember && o.Position.DistanceTo(t.Position) <=10) >= Settings.Current.SoloDestructionAOECount && Settings.Current.SoloDestructionUseAOE, RotationCombatUtil.BotTargetFast, checkLoS: true),
+            new RotationStep(new RotationSpell("Death Coil"), 3.8f, (s,t) => (Me.IsInGroup && BossList.isboss && t.HealthPercent > 20 && !t.IsStunned) || (Me.HealthPercent < 35 && !t.IsStunned), RotationCombatUtil.BotTarget),
             new RotationStep(new RotationSpell("Drain Life"), 3.9f, (s,t) => !Me.IsInGroup && Me.HealthPercent < 75, RotationCombatUtil.BotTarget),
-            new RotationStep(new RotationSpell("Immolate"), 4f, (s,t) => !t.HaveMyBuff("Immolate") && t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
-            new RotationStep(new RotationSpell("Corruption"), 5f, (s,t) => !t.HaveMyBuff("Corruption") && t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
-            new RotationStep(new RotationSpell("Chaos Bolt"), 6f, (s,t) => t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
-            new RotationStep(new RotationSpell("Incinerate"), 7f, (s,t) => t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
-            new RotationStep(new RotationSpell("Conflagrate"), 8f, (s,t) => t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
+            new RotationStep(new RotationSpell("Corruption"), 4f, (s,t) => !t.HaveMyBuff("Corruption") && t.HealthPercent > Settings.Current.UseWandTresh && t.IsElite, RotationCombatUtil.BotTarget),
+            new RotationStep(new RotationSpell("Immolate"), 5f, (s,t) => !t.HaveMyBuff("Immolate") && t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
+            new RotationStep(new RotationSpell("Conflagrate"), 6f, (s,t) => (t.HaveBuff("Immolate") || t.HaveBuff("Shadowflame")) && t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
+            new RotationStep(new RotationSpell("Chaos Bolt"), 7f, (s,t) => t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
+            new RotationStep(new RotationSpell("Incinerate"), 8f, (s,t) => t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
             new RotationStep(new RotationSpell("Shadow Bolt"), 9f, (s,t) => t.HealthPercent > Settings.Current.UseWandTresh, RotationCombatUtil.BotTarget),
         };
     }
