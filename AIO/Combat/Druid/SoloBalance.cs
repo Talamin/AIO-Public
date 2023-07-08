a55d515c1f02d6524d0bcbd45f860d53a08f4ddb
Michal Rozek <michalrozek90@gmail.com>
2023-07-08 22:50:43 +0200

    Tranquility moved to combat rotation so it can be used in combat

--- AIO/Combat/Druid/SoloBalance.cs
+++ AIO/Combat/Druid/SoloBalance.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using wManager.Wow.Enums;
 using wManager.Wow.Helpers;
+using wManager.Wow.ObjectManager;
 using static AIO.Constants;
 
 namespace AIO.Combat.Druid
@@ -14,12 +15,13 @@ namespace AIO.Combat.Druid
     {
         protected override List<RotationStep> Rotation => new List<RotationStep> {
             new RotationStep(new RotationSpell("Auto Attack"), 1f, (s,t) => !Me.IsCast && !RotationCombatUtil.IsAutoAttacking(), RotationCombatUtil.BotTarget),
-            new RotationStep(new RotationSpell("Regrowth"), 1.1f, (s,t) => !Me.HaveBuff("Regrowth") && Me.HealthPercent < Settings.Current.SoloBalanceRegrowth, RotationCombatUtil.FindMe),
-            new RotationStep(new RotationSpell("Healing Touch"), 1.2f, (s ,t) => Me.HealthPercent < Settings.Current.SoloBalanceHealingTouch, RotationCombatUtil.FindMe),
-            new RotationStep(new RotationSpell("Regrowth"), 1.3f, (s,t) => !t.HaveBuff("Regrowth") && t.HealthPercent < Settings.Current.SoloBalanceRegrowth, RotationCombatUtil.FindPartyMember),
-            new RotationStep(new RotationSpell("Healing Touch"), 1.4f, (s ,t) => t.HealthPercent < Settings.Current.SoloBalanceHealingTouch, RotationCombatUtil.FindPartyMember),
-            new RotationStep(new RotationBuff("Innervate"), 1.5f, (s,t) => Me.IsInGroup && t.Name == RotationFramework.HealName && t.ManaPercentage < Settings.Current.Innervate, RotationCombatUtil.FindPartyMember),
-            new RotationStep(new RotationBuff("Innervate"), 1.6f, (s,t) => Me.ManaPercentage <= Settings.Current.Innervate, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationBuff("Tranquility"), 1.1f, UseTranquility, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationSpell("Regrowth"), 1.2f, (s,t) => !Me.HaveBuff("Regrowth") && Me.HealthPercent < Settings.Current.SoloBalanceRegrowth, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationSpell("Healing Touch"), 1.3f, (s ,t) => Me.HealthPercent < Settings.Current.SoloBalanceHealingTouch, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationSpell("Regrowth"), 1.4f, (s,t) => !t.HaveBuff("Regrowth") && t.HealthPercent < Settings.Current.SoloBalanceRegrowth, RotationCombatUtil.FindPartyMember),
+            new RotationStep(new RotationSpell("Healing Touch"), 1.5f, (s ,t) => t.HealthPercent < Settings.Current.SoloBalanceHealingTouch, RotationCombatUtil.FindPartyMember),
+            new RotationStep(new RotationBuff("Innervate"), 1.6f, (s,t) => Me.IsInGroup && t.Name == RotationFramework.HealName && t.ManaPercentage < Settings.Current.Innervate, RotationCombatUtil.FindPartyMember),
+            new RotationStep(new RotationBuff("Innervate"), 1.7f, (s,t) => Me.ManaPercentage <= Settings.Current.Innervate, RotationCombatUtil.FindMe),
             new RotationStep(new RotationSpell("Moonkin Form"), 2f, (s, t) => !Me.HaveBuff("Moonkin Form"), RotationCombatUtil.FindMe),
             new RotationStep(new RotationBuff("Barkskin"), 3f, (s, t) => RotationFramework.Enemies.Count(o => o.IsTargetingMe && o.Position.DistanceTo(t.Position) <= 20) >= 2 || (!Me.IsInGroup && Me.InCombat), RotationCombatUtil.FindMe),
             new RotationStep(new RotationSpell("Abolish Poison"), 3.2f, (s,t) =>  !t.HaveMyBuff("Abolish Poison") && t.HaveImportantPoison(), RotationCombatUtil.FindPartyMember),
@@ -38,5 +40,17 @@ namespace AIO.Combat.Druid
             new RotationStep(new RotationSpell("Wrath"), 15f, (s, t) => Me.HaveBuff("Eclipse (Solar)") && Me.ManaPercentage > 5, RotationCombatUtil.BotTarget),
             new RotationStep(new RotationSpell("Wrath"), 16f, (s, t) => !Me.HaveBuff("Eclipse (Solar)") && !Me.HaveBuff("Eclipse (Lunar)"), RotationCombatUtil.BotTarget, checkLoS: true),
         };
+
+
+        private bool UseTranquility(IRotationAction s, WoWUnit t)
+        {
+            var nearbyFriendlies = RotationFramework.PartyMembers.Where(o => o.IsAlive && o.GetDistance <= 40).ToList();
+
+            var under40 = nearbyFriendlies.Count(o => o.HealthPercent <= 40);
+            var under55 = nearbyFriendlies.Count(o => o.HealthPercent <= 55);
+            var under65 = nearbyFriendlies.Count(o => o.HealthPercent <= 65);
+
+            return Me.IsInGroup && RotationFramework.PartyMembers.Count(u => u.IsAlive) >= 1 && (under40 >= 2 || under55 >= 3 || under65 >= 4);
+        }
     }
 }
